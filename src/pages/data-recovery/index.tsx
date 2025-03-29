4bd32962c1f0c92b910d2ad0cc034bd3d6de7cbc
mathiaslawson <mathiaslawson70@gmail.com>
2025-03-29 18:38:38 +0000

    update: data recovery

--- src/pages/data-recovery/index.tsx
+++ src/pages/data-recovery/index.tsx
@@ -1,7 +1,145 @@
 function DataRecovery() {
   return (
-    <div>DataRecovery</div>
-  )
+    <div className="min-h-screen bg-white justify-center flex">
+      {/* Main Content */}
+      <main className="max-w-7xl">
+        {/* Section 1 - First Column */}
+        <div className="flex flex-col md:flex-row">
+          <section className="p-20 bg-white md:w-1/2">
+            <div className="flex justify-center flex-col items-center">
+              <div className="flex justify-center flex-col items-center">
+                <div className="px-4 text-center mb-8">
+                  <img
+                    src={"../logos/logo5.avif"}
+                    alt={`logo`}
+                    className="w-[150px]"
+                  />
+                </div>
+                <h1 className="text-4xl font-bold text-red-700 mb-4 text-center">
+                  Data Recovery
+                </h1>
+
+                <p className="mt-2 text-neutral-800/60 text-lg leading-[30px] max-w-md text-center">
+                  <span className="text-red-700 text-2xl mr-3 font-bold">
+                    CTG
+                  </span>
+                  understand how valuable your data is — it's your livelihood,
+                  your memories, your connections and in worst-case scenarios
+                  your evidence — a lot of times it is all of the above. Which
+                  is why CTG has the best women and men in the field. Our team
+                  is constantly evolving to keep up with the newest technology,
+                  so that you can sleep easy at night knowing that even on the
+                  toughest cases we are working together to exhaust every
+                  possible option until we come up with the solution you need.
+                </p>
+
+                <div className="px-4 text-center mt-8">
+                  <img
+                    src={"../logos/logo4.avif"}
+                    alt={`logo`}
+                    className="w-[150px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
+
+          {/* Section 2 - Second Column */}
+          <section className="flex flex-col md:w-1/2">
+            <div className="p-6 flex flex-col items-center justify-center text-center mt-10">
+              <p className="font-bold text-2xl max-w-xl text-red-900">
+                World Class Data Recovery & Digital Forensics
+              </p>
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                <span className="text-red-700 text-2xl mr-3 font-bold">
+                  CTG
+                </span>
+                offers world class Digital Forensics & Data Recovery and s
+                ISO17025:2017 Accredited - The international gold standard,
+                equal to the US FBI and DEA. Our Digital Forensics capacity
+                supports Expert Testimony in civil courts or for Criminal
+                Prosecution. Our Data Recovery teams have recovered systems
+                damaged by natural disaster that other firms would not even
+                attempt. -
+              </p>
+
+              <div className="h-48 bg-slate-100 mt-5">
+                <img
+                  src="../mon.gif"
+                  alt="Proactive Security"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+
+              <p className="font-bold text-2xl max-w-xl text-red-700 mt-8">
+                Cutting Edge Technology to Recover Deleted Files, Corrupted
+                Data, & Damaged Media
+              </p>
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                <span className="text-red-700 text-2xl mr-3 font-bold">
+                  CTG
+                </span>
+                offers advanced solutions for data loss situations. We have the
+                technical experience to recover your critical data whether you
+                need to recover deleted files or recover lost or corrupt data
+                from damaged media.
+              </p>
+
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                We can step in and turn a potential catastrophe into a complete
+                recovery when IT departments, computer service companies, and
+                even other data recovery companies claim that your device is
+                unrecoverable and your data is lost.
+              </p>
+
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                We Provide A Superior Clean Room Environment
+              </p>
+
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                Our data recovery solutions are the most advanced in the
+                industry. Working in a Class 10 clean environment, our
+                technicians use the latest in-house technology to get your data
+                back quickly and effectively.
+              </p>
+
+              <div className="h-48 bg-slate-100 mt-5 mb-5">
+                <img
+                  src="../mon.gif"
+                  alt="Proactive Security"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+
+              <p className="font-bold text-2xl max-w-xl text-red-900">
+                World Class Data Recovery & Digital Forensics
+              </p>
+              <p className="mt-10 text-neutral-800/60 text-lg leading-[30px] max-w-xl">
+                <span className="text-red-700 text-2xl mr-3 font-bold">
+                  CTG
+                </span>
+                offers world class Digital Forensics & Data Recovery and s
+                ISO17025:2017 Accredited - The international gold standard,
+                equal to the US FBI and DEA. Our Digital Forensics capacity
+                supports Expert Testimony in civil courts or for Criminal
+                Prosecution. Our Data Recovery teams have recovered systems
+                damaged by natural disaster that other firms would not even
+                attempt. -
+              </p>
+
+              <div className="h-48 bg-slate-100">
+                <img
+                  src="../mon.gif"
+                  alt="Proactive Security"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default DataRecovery
+export default DataRecovery;
